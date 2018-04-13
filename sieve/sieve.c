dce9abfc063a0375b682a9a4de19e3401af26f1f
Ben Cradick <ben.cradick@gmail.com>
2018-04-13 13:18:43 -0500

    Sieve is done, need to make the make file still.

--- sieve/sieve.c
+++ sieve/sieve.c
@@ -4,43 +4,82 @@
 
 #include <stdio.h>
 #include <math.h>
-void
+#include <limits.h>
+
 
 int main() {
     // We have no use for negative numbers when dealing with primality, so unsigned everything.
     // accounts for different character sizes
-    unsigned short numIndex = 100000/CHAR_BIT;
-    unsigned char primes[numIndex] = {};
+
+    const unsigned int numIndex = (100000/CHAR_BIT)+1;
+
+    unsigned char primes[numIndex] = {0};
     unsigned long n = 100000;
-    unsigned short squareN = sqrt(n);
-    //initialize all bits to 0
-    for(int i = 0; i < numIndex; ++i){
-        primes[i]=0;
-    }
+    double squareN = sqrt(n);
+
     //pos = 1 is equivalent to a byte of CHAR_BIT length represented by '0..001'
-    unsigned char pos = 1;
+    unsigned char pos;
+
     //squareN is the highest you need to check for
-    for(int i = 2; i < squareN; ++i){
-        int index = i/CHAR_BIT;
+    for(unsigned int i = 2; i < squareN; i++){
+        pos = 1;
+        unsigned int j = i - 2;
+
+        int index = j/CHAR_BIT;
         // -1 is to compensate for starting at 2, we start at 2 because by definition 1 may not be prime.
-        int bit_pos = i%CHAR_BIT - 1;
-        while(index < numIndex){
-            //flips bit at wherever pos' bit is.
-            primes[index] |= pos;
+        unsigned int bit_pos = j%CHAR_BIT;
+        if(bit_pos != 0) {
+            pos <<= bit_pos;
+        }
+        while(index <= numIndex){
+
             //++variable was used where it wouldn't affect program logic, ++variable is faster than variable++
             //variable++ requires memory allocation/deallocation for the specified variable.
-            for(int k = 0; k < i; ++i){
-                pos<<=1;
-                if(pos == 0){
+            for(int k = 0; k < i; ++k){
+
+                if( pos != 128){
+                    pos<<=1;
+                }
+                else{
                     pos = 1;
                     ++index;
                 }
+
+
+
+
             }
-        }
-    }
-
-    for (int k = 2; k < n; ++k) {
-
+            //flips bit at wherever pos' bit is.
+            primes[index] |= pos;
 
         }
     }
+
+    pos = 1;
+    int index = 0;
+    int prints = 0;
+    int k = 2;
+
+    while (index <= numIndex) {
+
+
+        if(pos !=(primes[index] & pos)){
+
+            printf("%d\n", k);
+        }
+
+        if(pos == 128) {
+            pos = 1;
+            if ((index) < numIndex) { index++; }
+
+
+        }
+        else if(k >= n){
+
+            return 0;
+        }
+        else{pos<<=1;}
+
+        ++k;
+    }
+}
