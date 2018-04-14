acb22693ce040c0e5c43204979966dd3237efa5f
Ben Cradick <ben.cradick@gmail.com>
2018-04-14 16:22:56 -0500

    cleaned up some of the underlying code and made outputs human readable.

--- cards/shuffle.c
+++ cards/shuffle.c
@@ -10,6 +10,8 @@ typedef struct
 
 int compare(const void *, const void *);
 
+void printArray(card_t *);
+
 int main(){
     card_t deck[52];
     srand(time(0));
@@ -24,16 +26,17 @@ int main(){
         }
     }
 
+    //shuffles the deck
     for(unsigned short m; m < 52; m++){
         int r = rand()%52;
         card_t temp = *deck;
         *deck = *(deck + r);
         *(deck + r) = temp;
     }
+    printArray(deck);
+    printf("\v");
     qsort(deck, 52 , sizeof(card_t), compare);
-    for(unsigned short n; n < 52; n ++){
-        printf("%d\t%d\n", deck[n].suit, deck[n].number);
-    }
+    printArray(deck);
 }
 
 int compare(const void * a, const void * b){
@@ -41,7 +44,30 @@ int compare(const void * a, const void * b){
     card_t *cardiA = (card_t *)a;
     card_t *cardiB = (card_t *)b;
 
+    //returns the larger of the two numbers so qsort can do its magic.
     if(((int)cardiA->number) > ((int)cardiB->number)){return(1);}
     else if(((int)cardiA->number) < ((int)cardiB->number)){return(-1);}
     else {return(1);}
+}
+
+void printArray(card_t * deck){
+    for(unsigned short n; n < 52; n++){
+        //converts suite type to human readable form that corresponds with the enumerated type.
+        switch(deck[n].suit) {
+            case 0 :
+                printf("%-16s%d\n", "Clubs", deck[n].number);
+                break;
+            case 1:
+                printf("%-16s%d\n", "Diamonds", deck[n].number);
+                break;
+            case 2:
+                printf("%-16s%d\n", "Hearts", deck[n].number);
+                break;
+            case 3:
+                printf("%-16s%d\n", "Spades", deck[n].number);
+                break;
+            default:
+                printf("Joker");
+        }
+    }
 }
