c0b0d5080c506ec1e0bdcb7903e8480fea41eb37
Ben Cradick <ben.cradick@gmail.com>
2018-04-14 15:43:36 -0500

    made makefile and cleaned up the naming of the files to be compiled

--- cards/shuffle.c
+++ cards/shuffle.c
@@ -8,14 +8,14 @@ typedef struct
     suit_t       suit    : 2;
 } card_t;
 
-
+int compare(const void *, const void *);
 
 int main(){
     card_t deck[52];
     srand(time(0));
     int k = 0;
     while (k < 52) {
-        for (suit_t i = clubs; i <= 3; i++) {
+        for (suit_t i = 0; i <= 3; i++) {
             for (unsigned int j = 1; j <= 13; j++) {
                 deck[k].suit = i;
                 deck[k].number = j;
@@ -30,7 +30,18 @@ int main(){
         *deck = *(deck + r);
         *(deck + r) = temp;
     }
+    qsort(deck, 52 , sizeof(card_t), compare);
     for(unsigned short n; n < 52; n ++){
         printf("%d\t%d\n", deck[n].suit, deck[n].number);
     }
+}
+
+int compare(const void * a, const void * b){
+    //when you make jokes in your code that you know your prof won't get feelsbadman
+    card_t *cardiA = (card_t *)a;
+    card_t *cardiB = (card_t *)b;
+
+    if(((int)cardiA->number) > ((int)cardiB->number)){return(1);}
+    else if(((int)cardiA->number) < ((int)cardiB->number)){return(-1);}
+    else {return(1);}
 }
